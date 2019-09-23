d26c621c70ed9e638d19450f158d458b715aeabe
nshhhin <shinn070110@gmail.com>
2019-09-23 20:50:40 +0900

    :heavy_plus_sign: サンプルを追加

---
+++ 第一回/Sample/Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by 新納真次郎 on 2019/09/23.
+//  Copyright © 2019 新納真次郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello, World")
+    }
+
+
+}
+
